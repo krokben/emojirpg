28f62af725c2594f5acb1816342a5c652aa3ab7d
Tommy Svensson <tommykajsvensson@gmail.com>
2017-12-29 20:22:07 +0100

    add stats bar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,8 +8,31 @@ export default class Header extends Component {
   render() {
     return (
       <div {...bem()}>
-        <span {...bem('logo')}>emoji.rpg</span>
+        <span {...bem('logo')}>
+          <span role="img" aria-label="emoji">
+            🤣
+          </span>
+          .rpg
+        </span>
         <span {...bem('trigram')} onClick={this.props.appActions.toggleMenu}>☰</span>
+        <div {...bem('stats')}>
+          <div {...bem('stat')}>
+            <label {...bem('label-text', 'lvl')}>lvl</label>
+            <span {...bem('label-no')}>{this.props.hero.lvl}</span>
+          </div>
+          <div {...bem('stat')}>
+            <span {...bem('label-no')}>{this.props.hero.atk}</span>
+            <label {...bem('label-text')}>atk</label>
+          </div>
+          <div {...bem('stat')}>
+            <span {...bem('label-no')}>{this.props.hero.def}</span>
+            <label {...bem('label-text')}>def</label>
+          </div>
+          <div {...bem('stat')}>
+            <span {...bem('label-no')}>{this.props.hero.hp}</span>
+            <label {...bem('label-text')}>hp</label>
+          </div>
+        </div>
       </div>
     );
   }
